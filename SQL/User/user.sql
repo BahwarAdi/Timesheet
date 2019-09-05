caef3a45de89bda729494830412c3fe9d41d88e3
Bahwar <bahwar.adi@reamis.ch>
2019-09-05 09:49:39 +0200

    Neue SQL

--- SQL/User/user.sql
+++ SQL/User/user.sql
@@ -12,6 +12,7 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 -- Exportiere Struktur von Tabelle Timesheet.user
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `userId` int(11) NOT NULL AUTO_INCREMENT,
   `nachname` varchar(50) NOT NULL,
@@ -21,14 +22,9 @@ CREATE TABLE IF NOT EXISTS `user` (
   `typ` varchar(50) NOT NULL,
   `soll` time DEFAULT NULL,
   PRIMARY KEY (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
--- Exportiere Daten aus Tabelle Timesheet.user: ~2 rows (ungefähr)
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`userId`, `nachname`, `vorname`, `email`, `passwort`, `typ`, `soll`) VALUES
-	(1, 'admin', 'admin', 'testadmin@admin.com', '12345', 'admin', NULL),
-	(2, 'Test', 'User', 'TestUser@test.com', '12345', 'user', '08:20:00');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+-- Daten Export vom Benutzer nicht ausgewählt
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
--- SQL/timesheet.sql
+++ SQL/timesheet.sql
@@ -17,24 +17,16 @@ DROP DATABASE IF EXISTS `Timesheet`;
 CREATE DATABASE IF NOT EXISTS `Timesheet` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `Timesheet`;
 
--- Exportiere Struktur von Tabelle Timesheet.projekt
-DROP TABLE IF EXISTS `projekt`;
-CREATE TABLE IF NOT EXISTS `projekt` (
+-- Exportiere Struktur von Tabelle Timesheet.Projekt
+DROP TABLE IF EXISTS `Projekt`;
+CREATE TABLE IF NOT EXISTS `Projekt` (
   `projektId` int(11) NOT NULL AUTO_INCREMENT,
-  `projektname` varchar(50) NOT NULL,
-  `beschreibung` tinytext NOT NULL,
-  `archiviert` tinytext NOT NULL,
-  PRIMARY KEY (`projektId`),
-  UNIQUE KEY `Schlüssel 2` (`projektname`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+  `projektname` varchar(50) DEFAULT NULL,
+  `beschreibung` tinytext,
+  PRIMARY KEY (`projektId`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Exportiere Daten aus Tabelle Timesheet.projekt: ~3 rows (ungefähr)
-/*!40000 ALTER TABLE `projekt` DISABLE KEYS */;
-INSERT INTO `projekt` (`projektId`, `projektname`, `beschreibung`, `archiviert`) VALUES
-	(1, 'TestProjekt', 'ein TestProjekt', 'false'),
-	(2, 'Telefon', 'telefon in session', 'false'),
-	(3, 'Salat', 'Salat Jonglieren', 'true');
-/*!40000 ALTER TABLE `projekt` ENABLE KEYS */;
+-- Daten Export vom Benutzer nicht ausgewählt
 
 -- Exportiere Struktur von Tabelle Timesheet.user
 DROP TABLE IF EXISTS `user`;
@@ -47,37 +39,24 @@ CREATE TABLE IF NOT EXISTS `user` (
   `typ` varchar(50) NOT NULL,
   `soll` time DEFAULT NULL,
   PRIMARY KEY (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
--- Exportiere Daten aus Tabelle Timesheet.user: ~2 rows (ungefähr)
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`userId`, `nachname`, `vorname`, `email`, `passwort`, `typ`, `soll`) VALUES
-	(1, 'admin', 'admin', 'testadmin@admin.com', '12345', 'admin', NULL),
-	(2, 'Test', 'User', 'TestUser@test.com', '12345', 'user', '08:20:00');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+-- Daten Export vom Benutzer nicht ausgewählt
 
 -- Exportiere Struktur von Tabelle Timesheet.zeit
 DROP TABLE IF EXISTS `zeit`;
 CREATE TABLE IF NOT EXISTS `zeit` (
   `zeitId` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL,
-  `projektId` int(11) NOT NULL,
   `kw` int(11) NOT NULL,
   `datum` date NOT NULL,
   `start` time NOT NULL,
   `stop` time NOT NULL,
   `pause` time DEFAULT NULL,
-  `beschreibung` tinytext NOT NULL,
-  PRIMARY KEY (`zeitId`),
-  KEY `FK_zeit_user` (`userId`),
-  KEY `FK_zeit_projekt` (`projektId`),
-  CONSTRAINT `FK_zeit_projekt` FOREIGN KEY (`projektId`) REFERENCES `projekt` (`projektId`),
-  CONSTRAINT `FK_zeit_user` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`)
+  PRIMARY KEY (`zeitId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Exportiere Daten aus Tabelle Timesheet.zeit: ~0 rows (ungefähr)
-/*!40000 ALTER TABLE `zeit` DISABLE KEYS */;
-/*!40000 ALTER TABLE `zeit` ENABLE KEYS */;
+-- Daten Export vom Benutzer nicht ausgewählt
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
