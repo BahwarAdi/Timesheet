80c3d887788d8b86c3004bfc18c3d8fe53fd941d
MikeGlauserReamis <mike.glauser@reamis.ch>
2019-09-09 08:50:50 +0200

    Anpassung system.php

--- Admin/System/system.php
+++ Admin/System/system.php
@@ -2,6 +2,9 @@
 session_start();
 require_once('../../Config/config.php');
 
+if (isset($_POST['main'])) {
+    header('Location: ../../Pages/Mainpage.php');
+}
 #region verarbeitung
 #region verarbeitung soll
 if (isset($_POST['soll'])) {
@@ -46,147 +49,152 @@ elseif (isset($_POST['delFeiertag'])) {
 <body>
 <div class="cont">
     <div class="fc">
-        <fieldset>
-            <h2>System</h2>
-            <div class="bls">
-                <div>
-                    <form action="" method="post">
-                        <h3>Konfiguration Tagessoll</h3>
+        <form action="" method="POST">
+            <fieldset>
+                <h2>System</h2>
+                <div class="bls">
+                    <div>
+                        <form action="" method="post">
+                            <h3>Konfiguration Tagessoll</h3>
+                            <table>
+                                <tr>
+                                    <td>
+                                        Nachname User:
+                                    </td>
+                                    <td>
+                                        <input type="text" name="nachname">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        Vorname User:
+                                    </td>
+                                    <td>
+                                        <input type="text" name="vorname">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        Tagessoll:
+                                    </td>
+                                    <td>
+                                        <input type="time" name="tagessoll">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <input type="submit" name="soll" value="Festlegen">
+                                    </td>
+                                    <td>
+                                        <input type="reset">
+                                    </td>
+                                </tr>
+                            </table>
+                        </form>
+                    </div>
+                    <div>
+                        <form action="" method="post">
+                            <h3>Feiertag Hinzufügen</h3>
+                            <table>
+                                <tr>
+                                    <td>
+                                        Name:
+                                    </td>
+                                    <td>
+                                        <input type="text" name="feiertagName">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        Datum:
+                                    </td>
+                                    <td>
+                                        <input type="date" name="feiertagDatum">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        Arbeitszeit:
+                                    </td>
+                                    <td>
+                                        <input type="time" name="feiertagZeit">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <input type="submit" name="addFeiertag" value="Hinzufügen">
+                                    </td>
+                                    <td>
+                                        <input type="reset">
+                                    </td>
+                                </tr>
+                            </table>
+                        </form>
+                    </div>
+                    <div>
+                        <form action="" method="post">
+                            <h3>Feiertag Entfernen</h3>
+                            <table>
+                                <tr>
+                                    <td>
+                                        Id:
+                                    </td>
+                                    <td>
+                                        <input type="number" name="feiertagId">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <input type="submit" name="delFeiertag" value="Entfernen">
+                                    </td>
+                                    <td>
+                                        <input type="reset">
+                                    </td>
+                                </tr>
+                            </table>
+                        </form>
+                    </div>
+                    <div>
                         <table>
                             <tr>
-                                <td>
-                                    Nachname User:
-                                </td>
-                                <td>
-                                    <input type="text" name="nachname">
-                                </td>
+                                <th>
+                                    Id
+                                </th>
+                                <th>
+                                    Feiertag
+                                </th>
+                                <th>
+                                    Datum
+                                </th>
+                                <th>
+                                    Arbeitszeit
+                                </th>
                             </tr>
-                            <tr>
-                                <td>
-                                    Vorname User:
-                                </td>
-                                <td>
-                                    <input type="text" name="vorname">
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    Tagessoll:
-                                </td>
-                                <td>
-                                    <input type="time" name="tagessoll">
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <input type="submit" name="soll" value="Festlegen">
-                                </td>
-                                <td>
-                                    <input type="reset">
-                                </td>
-                            </tr>
-                        </table>
-                    </form>
-                </div>
-                <div>
-                    <form action="" method="post">
-                        <h3>Feiertag Hinzufügen</h3>
-                        <table>
-                            <tr>
-                                <td>
-                                    Name:
-                                </td>
-                                <td>
-                                    <input type="text" name="feiertagName">
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    Datum:
-                                </td>
-                                <td>
-                                    <input type="date" name="feiertagDatum">
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    Arbeitszeit:
-                                </td>
-                                <td>
-                                    <input type="time" name="feiertagZeit">
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <input type="submit" name="addFeiertag" value="Hinzufügen">
-                                </td>
-                                <td>
-                                    <input type="reset">
-                                </td>
-                            </tr>
-                        </table>
-                    </form>
-                </div>
-                <div>
-                    <form action="" method="post">
-                        <h3>Feiertag Entfernen</h3>
-                        <table>
-                            <tr>
-                                <td>
-                                    Id:
-                                </td>
-                                <td>
-                                    <input type="number" name="feiertagId">
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <input type="submit" name="delFeiertag" value="Entfernen">
-                                </td>
-                                <td>
-                                    <input type="reset">
-                                </td>
-                            </tr>
-                        </table>
-                    </form>
-                </div>
-                <div>
-                    <table>
-                        <tr>
-                            <th>
-                                Id
-                            </th>
-                            <th>
-                                Feiertag
-                            </th>
-                            <th>
-                                Datum
-                            </th>
-                            <th>
-                                Arbeitszeit
-                            </th>
-                        </tr>
 
-                        <?php
+                            <?php
 
-                        $res = $mysqli->query("SELECT feiertagId,feiertagName, datum,arbeitszeit FROM feiertag ORDER BY feiertagId");
+                            $res = $mysqli->query("SELECT feiertagId,feiertagName, datum,arbeitszeit FROM feiertag ORDER BY feiertagId");
 
-                        while ($row = $res->fetch_assoc()) {
+                            while ($row = $res->fetch_assoc()) {
 
-                            echo('<tr>
+                                echo('<tr>
           <td>' . $row['feiertagId'] . '</td>
           <td>' . $row['feiertagName'] . '</td>
           <td>' . $row['datum'] . '</td>
           <td>' . $row['arbeitszeit'] . '</td>
           </tr>');
 
-                        }
-                        ?>
+                            }
+                            ?>
 
-                    </table>
+                        </table>
+                    </div>
+                    <div>
+                        <button type="submit" name="main"> Hauptseite</button>
+                    </div>
                 </div>
-            </div>
-        </fieldset>
+            </fieldset>
+        </form>
     </div>
 </div>
 </body>
