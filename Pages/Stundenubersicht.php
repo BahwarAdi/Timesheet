5e1c9a7b7d9cfb32452e42c21ca73019019c8a6b
Simon Bertolf <simon.bertolf@reamis.ch>
2019-09-11 12:38:30 +0200

    Stundenanzeigen gemacht

--- Pages/Stundenubersicht.php
+++ Pages/Stundenubersicht.php
@@ -6,6 +6,9 @@ if ($_POST['change'] == 'main'){
 if ($_POST['change'] == 'time'){
     header('Location: ../Zeiterfassung/Zeiterfassung.php');
 }
+if ($_POST['change'] == 'timeshow'){
+    header('Location: ../Zeiterfassung/Stunden.php');
+}
 ///----- Dateien Holen -----///
 require_once "../Config/config.php";
 require_once "../Zeiterfassung/Class_Zeit.php";
@@ -86,6 +89,7 @@ $userid = $_SESSION['userId'];
 
                     <button type="submit" name="change" value="main"> zur Hauptseite zurück </button>
                     <button type="submit" name="change" value="time"> Stunden erfassen </button>
+                    <button type="submit" name="change" value="timeshow"> Stunden Anzeigen </button>
 
 
         </form>
