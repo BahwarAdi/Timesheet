d4e2951872a8d024347ef49236bea62fe9a68809
Simon Bertolf <simon.bertolf@reamis.ch>
2019-09-17 13:14:45 +0200

    Export Speichern hinzugefügt

--- Export/Export.php
+++ Export/Export.php
@@ -2,20 +2,24 @@
 session_start();
 require_once "../Config/config.php";
 require_once "../Zeiterfassung/Class_Zeit.php";
+date_default_timezone_set("Europe/Berlin");
+$timestamp = time();
+
 $z = new Zeit();
 
 $titel = array('Vorname', 'Nachname', 'Projektname', 'Datum', 'Startzeit', 'Endzeit', 'Pausen', 'Arbeitszeit');
 $stunden = array();
 
-$comm = "SELECT user.vorname, user.nachname, projekt.projektname, zeit.datum, zeit.`start`,zeit.`stop`, zeit.pause FROM zeit 
+$comm = "SELECT user.vorname, user.nachname, projekt.projektname, zeit.datum, zeit.`start`,zeit.`stop`, zeit.pause FROM zeit
 LEFT JOIN user ON zeit.userId = user.userId
-LEFT JOIN projekt ON zeit.projektId = projekt.projektId 
+LEFT JOIN projekt ON zeit.projektId = projekt.projektId
 WHERE datum BETWEEN '".$_POST['von']."' AND '".$_POST['bis']."'
 ORDER BY datum";
 
-echo ($_POST['von']);
+
 
 if (isset($_POST['go'])){
+
 
     $query = $mysqli->query($comm);
     $rows = array();
@@ -34,16 +38,14 @@ if (isset($_POST['go'])){
             $z->arbeitszeitcharts($text['start'], $text['stop'], $text['pause'])
         );
     }
-    $fp = fopen('../daten.csv', 'w');
+
+    $fp = fopen('../Stundenexport.csv', 'w');
     fputcsv($fp, $titel);
     foreach ($stunden as  $text) {
         fputcsv($fp, $text);
     }
     fclose($fp);
 }
-
-
-
 ?>
 
 <html xmlns="http://www.w3.org/1999/xhtml" lang='de'>
@@ -67,7 +69,9 @@ if (isset($_POST['go'])){
                     <div class='bls'>
                         <input type="date" name="von" >
                         <input type="date" name="bis" >
-                        <button type="submit" name="go">Exportieren</button>
+                        <button type="submit" name="go" >Exportieren</button>
+                        <button><a href="../Stundenexport.csv">Speichern</a></button>
+
                     </div>
             </fieldset>
         </form>
